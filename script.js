b3bae310e5b560f23b97d366da28fca69832ced3
seannovela1 <seannovela5484@my.whccd.edu>
2024-11-10 11:24:11 -0800

    added function range

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // 1. Write a range function that takes two arguments, start and end, and returns an array
 // containing all the numbers from start up to (and including) end.
-// Example output:
-// console.log(range(1, 10)); // outputs [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+function range(start, end) {
+    let array = [];
+    for (let i = start; i <= end; i++) {
+        array.push(i);
+    }
+    return array;
+  }
+ console.log(range(1, 10)); // outputs [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // 2. Next, write a sum function that takes an array of numbers and returns the sum of
 // these numbers. Run the example program and see whether it does indeed return 55.
